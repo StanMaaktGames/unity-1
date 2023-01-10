f767477f7d4fdf136dcefe458b30649fd8d2008a
StanMaaktGames <100526568+StanMaaktGames@users.noreply.github.com>
2023-01-10 10:14:06 +0100

    code van challenge

---
+++ PlayerControllerX.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public float speed;
+    public float rotationSpeed;
+    public float verticalInput;
+
+    void FixedUpdate()
+    {
+        // get the user's vertical input
+        verticalInput = Input.GetAxis("Vertical");
+
+        // move the plane forward at a constant rate
+        transform.Translate(Vector3.forward * speed);
+
+        // tilt the plane up/down based on up/down arrow keys
+        transform.Rotate(Vector3.right * verticalInput * Time.deltaTime * rotationSpeed);
+    }
+
+    void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.CompareTag("obstacle"))
+        {
+            transform.position = new Vector3(0, 0, 0);
+            transform.rotation = Quaternion.identity;
+            GetComponent<Rigidbody>().velocity = Vector3.zero;            
+        }
+    }
+}
